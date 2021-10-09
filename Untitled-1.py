3550c1aff11782dfa7fcbacc207c852e23e7a2de
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-10-09 10:55:20 -0600

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+def resta(n1,n2):
+    if n1>n2:
+        return n1-n2
+    else:
+        return 0
+ 
+capturadatosdefuncion = resta(6,1)
+
+if capturadatosdefuncion ==0:
+    print("No queremos mostrar numeros negativos")
+else:
+    print(capturadatosdefuncion)
